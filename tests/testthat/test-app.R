f602ba9152659a62e435768f0e6ce76f780532de
xari <harry@tsf-444-wpa-3-148.epfl.ch>
2020-08-28 14:11:19 +0200

    added test file updates

--- tests/testthat/test-app.R
+++ tests/testthat/test-app.R
@@ -1,6 +1,8 @@
+library(leaflet)
+
 test_that("Connections are returned", {
-  trip_details = list(from = "Geneva",
-                      to = "Basel")
+  trip_details = list(from = "8500090",
+                      to = "8501008")
 
   url <- do.call(create_url_from_args, trip_details)
 
@@ -12,5 +14,26 @@ test_that("Connections are returned", {
     connections_tibble %>%
     dplyr::mutate(sections = create_tibble_from_sections(sections))
 
+  readr::write_rds(connections_tibble,
+                   path = "./example_connection_gva_bsl.rds")
 
+  # For GT
+  connections_tibble %>%
+    dplyr::slice(1) %>%
+    purrr::pluck("sections") %>%
+    dplyr::first() %>%
+    dplyr::select(-stops) %>%
+    dplyr::mutate(
+      walk = ifelse(!is.na(walk),
+                    paste(walk / 60, "minute walk"),
+                    NA),
+      product = paste(category, number),
+      description = ifelse(is.na(walk),
+                           product,
+                           walk)
+    ) %>%
+    dplyr::select(-category,-number,-walk, -product)
+
+  hoist_stops_from_connections(connections_tibble, 3) %>%
+    get_map()
 })
